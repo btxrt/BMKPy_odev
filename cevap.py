ed6f8c21fa6bc3fa8219eb9217dd867eaf8eb8e2
btxrt <b.kurt1771@gmail.com>
2024-12-01 17:59:47 +0300

    cevapların eklenmesi

---
+++ cevap.py
@@ -0,0 +1,24 @@
+markalar=["Toyoto","BMW","Renault","Mercedes"]
+
+elemanSayısı=len(markalar)
+print(elemanSayısı)
+
+print(markalar[0]+" "+markalar[3])
+
+
+markalar[2]="Togg"
+print(markalar)
+
+print("bence hayırr")
+
+markalar.append("Ford")
+markalar.append("Citroen")
+print(markalar)
+
+
+markalar.pop()
+print(markalar)
+
+
+
+
