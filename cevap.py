8df378c7438671d88f3f133574c5489d97b49f45
btxrt <b.kurt1771@gmail.com>
2024-12-02 22:47:19 +0300

    update2

--- cevap.py
+++ cevap.py
@@ -20,6 +20,26 @@ print(markalar)
 markalar.pop()
 print(markalar)
 
+ogrenci1=["Yiğit", "Bilgi", 2010, [70,80,90]]
+ogrenci2=["Ada", "Bilgi", 2011, [70,70,80]]
+ogrenci3=["Çınar", "Turan", 2017, [60,60,90]]
 
 
 
+ogrenci1yas=2024-ogrenci1[2]
+ogrenci2yas=2024-ogrenci2[2]
+ogrenci3yas=2024-ogrenci3[2]
+
+
+print(ogrenci1yas)
+print(ogrenci2yas)
+print(ogrenci3yas)
+
+
+ogrenci1ortalama=(ogrenci1[3][0] + ogrenci1[3][1]+ ogrenci1[3][2])/3
+ogrenci2ortalama=(ogrenci2[3][0]+ ogrenci2[3][1]+ ogrenci2[3][2])/3
+ogrenci3ortalama=(ogrenci3[3][0]+ ogrenci3[3][1]+ ogrenci3[3][2])/3
+
+print(ogrenci1ortalama)
+print(ogrenci2ortalama)
+print(ogrenci3ortalama)
